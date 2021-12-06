85b6dbde5e0c25d12b2655db9bc0c0b9bd23d1bd
hojunkimdev <hojunkimdev@gmail.com>
2021-12-06 17:10:16 +0900

    Change path

--- DP/금광/main.py
+++ DP/금광/main.py
@@ -1,26 +1,26 @@
 # 테스트 케이스(Test Case) 입력
 for tc in range(int(input())):
     # 금광 정보 입력
-    n, m = map(int, input().split())
+    row, col = map(int, input().split())
     array = list(map(int, input().split()))
 
     # 다이나믹 프로그래밍을 위한 2차원 DP 테이블 초기화
     dp = []
     index = 0
-    for i in range(n):
-        dp.append(array[index:index + m])
-        index += m
+    for i in range(row):
+        dp.append(array[index:index + col])
+        index += col
 
     # 다이나믹 프로그래밍 진행
-    for j in range(1, m):
-        for i in range(n):
+    for j in range(1, col):
+        for i in range(row):
             # 왼쪽 위에서 오는 경우
             if i == 0:
                 left_up = 0
             else:
                 left_up = dp[i - 1][j - 1]
             # 왼쪽 아래에서 오는 경우
-            if i == n - 1:
+            if i == row - 1:
                 left_down = 0
             else:
                 left_down = dp[i + 1][j - 1]
@@ -29,7 +29,7 @@ for tc in range(int(input())):
             dp[i][j] = dp[i][j] + max(left_up, left_down, left)
 
     result = 0
-    for i in range(n):
-        result = max(result, dp[i][m - 1])
+    for i in range(row):
+        result = max(result, dp[i][col - 1])
 
     print(result)
