92d8eaa70a1c5f74d2d73948f7ba2ae85beea45b
hojunkimdev <hojunkim.dev@gmail.com>
2022-04-05 22:26:17 +0900

    [Programmers] Lv1 - 로또의 최고 순위와 최저 순위

---
+++ Programmers/level1/로또의 최고 순위와 최저 순위.py
@@ -0,0 +1,21 @@
+def solution(lottos, win_nums):
+    answer = []
+    rank = [6, 6, 5, 4, 3, 2, 1]
+    corrects = 0
+    zero_cnt = lottos.count(0)
+
+    for num in win_nums:
+        if num in lottos:
+            corrects += 1
+
+    best = corrects + zero_cnt
+    worst = corrects
+
+    answer.append(rank[best])
+    answer.append(rank[worst])
+
+    return answer
+
+lottos = [44, 1, 0, 0, 31, 25]
+win_nums = [31, 10, 45, 1, 6, 19]
+print(solution(lottos, win_nums))
