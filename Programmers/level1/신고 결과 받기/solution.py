6ca78b6c3c0dcc714398bd2aecd205d416485791
hojunkimdev <hojunkim.dev@gmail.com>
2022-04-05 21:35:00 +0900

    [Programmers] Lv1 - 신고 결과 받기

---
+++ Programmers/level1/신고 결과 받기/solution.py
@@ -0,0 +1,24 @@
+# https://programmers.co.kr/learn/courses/30/lessons/92334
+
+def solution(id_list, report, k):
+    answer = [0] * len(id_list)
+    reports = {x: 0 for x in id_list}
+
+    for r in set(report):
+        reports[r.split()[1]] += 1
+
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
+
+    return answer
+
+id_list = ["muzi", "frodo", "apeach", "neo"]
+report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
+k = 2
+print(solution(id_list, report, k))
+
+id_list = ["con", "ryan"]
+report = ["ryan con", "ryan con", "ryan con", "ryan con"]
+k = 3
+print(solution(id_list, report, k))
